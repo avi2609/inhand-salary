68cf6378b73c101ec123225688ad26a7f52e4cbd
Avinash <avi45133@gmail.com>
2025-02-01 20:26:36 +0530

    Logic

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,106 @@ export default function Home() {
   const [monthlyPayment, setMonthlyPayment] = useState(0);
   const [selected, setSelected] = useState('curr');
   const [basePay, setBasePay] = useState(0);
+  const standardDeduction = 75000;
 
   const handleCalculate = () => {
     try {
-      // setMonthlyPayment()
+      let tax = taxCalulationWithSurcharge(basePay-standardDeduction);
+      tax *= 1.04;//Adding health & cess
+      const pfDeduction = Math.round(basePay * 0.4 * 0.12);
+      console.log('tax pf',tax, pfDeduction);
+      const monthlyInhand = Math.round((basePay - tax - pfDeduction - 2500) / 12);
+      setMonthlyPayment(monthlyInhand);
     } catch (error) {
       console.error(error);
     }
   };
 
+  function taxCalulationWithSurcharge(taxableIncome:number){
+    let tax = selected==='curr'?totalTax2025(taxableIncome):totalTax2024(taxableIncome);
+    if(taxableIncome > 20000000){
+      const taxOn2Cr = taxCalulationWithSurcharge(20000000);
+      const amtAbove2Cr = taxableIncome - 20000000;
+      if(amtAbove2Cr< (tax*1.25- taxOn2Cr)){
+        tax = taxOn2Cr + amtAbove2Cr;
+      }
+      else{
+        tax=tax*1.25;
+      }
+    }
+    else if(taxableIncome > 10000000){
+      const taxOn1Cr = taxCalulationWithSurcharge(10000000);
+      const amtAbove1Cr = taxableIncome - 10000000;
+      if(amtAbove1Cr< (tax*1.15- taxOn1Cr)){
+        tax = taxOn1Cr + amtAbove1Cr;
+      }
+      else{
+        tax=tax*1.15;
+      }
+    }
+    else if(taxableIncome> 5000000) {
+        let taxOn50 = selected==='curr'?totalTax2025(5000000):totalTax2024(5000000);
+        const amountAbove50Lakhs = taxableIncome - 5000000;
+        if(amountAbove50Lakhs < (tax*1.1 - taxOn50)){
+            tax = taxOn50 + amountAbove50Lakhs;
+        }
+        else
+            tax *=1.1;
+    }
+    return tax;
+  }
+
+  function totalTax2025(income:number){
+    let tax = 0;
+    if (income > 2400000) {
+      tax += (income - 2400000) * 0.3;
+      income = 2400000;
+    }
+    if (income > 2000000) {
+      tax += (income - 2000000) * 0.25;
+      income = 2000000;
+    }
+    if (income > 1600000) {
+      tax += (income - 1600000) * 0.20;
+      income = 1600000;
+    }
+    if (income > 1200000) {
+      tax += (income - 1200000) * 0.15;
+      income = 1200000;
+    }
+    if (income > 800000) {
+      tax += (income - 800000) * 0.1;
+      income = 800000;
+    }
+    if (income > 400000) {
+      tax += (income - 400000) * 0.05;
+    }
+    return tax;
+  }
+
+  function totalTax2024(income:number){
+    let tax = 0;
+    if (income > 1500000) {
+      tax += (income - 1500000) * 0.3;
+      income = 1500000;
+    }
+    if (income > 1200000) {
+      tax += (income - 1200000) * 0.2;
+      income = 1200000;
+    }
+    if (income > 1000000) {
+      tax += (income - 1000000) * 0.15;
+      income = 1000000;
+    }
+    if (income > 700000) {
+      tax += (income - 700000) * 0.1;
+      income = 700000;
+    }
+    if (income > 300000) {
+      tax += (income - 300000) * 0.05;
+    }
+    return tax;
+  }
   return (
     <div className=" flex flex-col justify-center items-center gap-2 max-sm:min-h-screen max-sm:bg-[#f1f3f6]">
       <div className="mt-4 text-xl font-medium">Monthly Take Home Salary Calculator</div>
@@ -64,7 +155,13 @@ export default function Home() {
             type="number"
             value={basePay}
             onChange={(e) => {
-              setBasePay(Number(e.target.value));
+              console.log("Base Pay", e.target.value);
+              setBasePay((e.target.value.replace(/^0+/, "")));
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleCalculate();
+              }
             }}
             className="w-full border border-gray-400 rounded-lg p-3 text-lg font-semibold h-10 bg-[#f1f3f6] text-[#264653]"
           />
